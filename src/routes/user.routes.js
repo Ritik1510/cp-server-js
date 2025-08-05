acea3049509b426627af4d3b9e119f839ab08d5f
ritik kumar <kr.ritik1510@gmail.com>
2025-08-05 09:09:08 +0530

    adding middleware for upload files

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,20 @@
 import { Router } from "express";
 import registerUser from "../controllers/user.controllers.js";
+import {upload} from "../middlewares/multer.middleware.js";
 
 const router = Router();
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.fields([ // middleware to handle multiple file uploads
+        // jate hoye ham se milte jana 
+        {
+            name: "avatar", 
+            maxCount: 1
+        }, 
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]), 
+    registerUser
+)
 export default router; 
