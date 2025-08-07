8ebf2ba2e39b3d0d3299a1257219535f3bef414c
ritik kumar <kr.ritik1510@gmail.com>
2025-08-07 13:07:25 +0530

    add 2 new routes login and logout as secured route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,20 +1,30 @@
 import { Router } from "express";
-import registerUser from "../controllers/user.controllers.js";
-import {upload} from "../middlewares/multer.middleware.js";
+import {
+    loginUser,
+    logoutUser,
+    registerUser,
+} from "../controllers/user.controllers.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 router.route("/register").post(
-    upload.fields([ // middleware to handle multiple file uploads
-        // jate hoye ham se milte jana 
+    upload.fields([
+        // middleware to handle multiple file uploads
+        // jate hoye ham se milte jana
         {
-            name: "avatar", 
-            maxCount: 1
-        }, 
+            name: "avatar",
+            maxCount: 1,
+        },
         {
             name: "coverImage",
-            maxCount: 1
-        }
-    ]), 
+            maxCount: 1,
+        },
+    ]),
     registerUser
-)
-export default router; 
+);
+
+router.route("/login").post(loginUser);
+
+// secured routes
+router.route("/logout").post(verifyJWT, logoutUser);
+export default router;
