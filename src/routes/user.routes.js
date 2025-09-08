1f36ccbd9e00ebb8e82096a1a6bfc6f33f73ecea
ritik kumar <kr.ritik1510@gmail.com>
2025-09-08 08:36:38 +0530

    remove the multer middle ware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,22 +6,9 @@ import {
     registerUser,
     refreshAccessToken
 } from "../controllers/user.controllers.js";
-import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 router.route("/register").post(
-    upload.fields([
-        // middleware to handle multiple file uploads
-        // jate hoye ham se milte jana
-        {
-            name: "avatar",
-            maxCount: 1,
-        },
-        {
-            name: "coverImage",
-            maxCount: 1,
-        },
-    ]),
     registerUser
 );
 
