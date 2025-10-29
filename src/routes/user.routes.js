d46c94a6ba5ec6d820b64436e5d3862d792a9aaa
ritik1510 <kr.ritik1510@gmail.com>
2025-10-29 11:50:27 +0530

    improve spacing

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {verifyJWT} from "../middlewares/auth.middleware.js" 
+import { verifyJWT } from "../middlewares/auth.middleware.js" 
 import {
     loginUser,
     logoutUser,
@@ -18,4 +18,5 @@ router.route("/login").get(loginUser);
 // ----------------------------------------secured routes--------------------------------------
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+
 export default router;
